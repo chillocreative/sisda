071bfd38d2d00db2edb7be0cd5a3d9298a40ebb5
hanafiabdilah <handilah81@gmail.com>
2021-12-17 05:49:45 +0700

    validation

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Role;
 use Illuminate\Http\Request;
 use App\Models\User;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class UserController extends Controller
@@ -11,8 +13,12 @@ class UserController extends Controller
     public function store(Request $request){
         $checkNoKad = User::where('no_kad', $request->no_kad)->first();
         $checkEmail = User::where('email', $request->email)->first();
+        $checkMaster = Role::find($request->role_id)->first();
         if(!$checkNoKad){
             if(!$checkEmail){
+                if(!Auth::user()->role->name == 'master' && $checkMaster == 'master'){
+                    return back()->with('error', 'Cant create master user');
+                }
                 $request['password'] = Hash::make($request->password);
                 User::create($request->all());
                 return back()->with('success', 'Successfully added user');
