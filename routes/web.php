b3a4b39f004a554e353b0321837a600415df8d97
Chillocreative <chillocreative@gmail.com>
2025-12-03 15:35:23 +0800

    Detach login page and create new welcome page

--- routes/web.php
+++ routes/web.php
@@ -6,7 +6,10 @@ use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
 Route::get('/', function () {
-    return redirect()->route('login');
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+    ]);
 });
 
 Route::get('/dashboard', [\App\Http\Controllers\DashboardController::class, 'index'])
