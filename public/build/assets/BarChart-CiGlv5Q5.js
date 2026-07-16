4e27fd9f7c47446d696495a265feb765b1a8bad4
Chillocreative <chillocreative@gmail.com>
2026-07-17 01:57:39 +0800

    Bina semula aset frontend selepas pembaikan Borang 14
    npm run build selepas dapatan 3 (UploadTab.jsx paparkan jenis kawasan yang
    diselesaikan pada panel pengesahan sebelum menulis).

---
+++ public/build/assets/BarChart-CiGlv5Q5.js
@@ -0,0 +1,1 @@
+import{r as a}from"./app-BP7QhjYY.js";import{y as e}from"./CategoricalChart-5oR_YBxQ.js";import{a as o}from"./CartesianChart-JqbFxF7a.js";var i=["axis","item"],m=a.forwardRef((r,t)=>a.createElement(o,{chartName:"BarChart",defaultTooltipEventType:"axis",validateTooltipEventTypes:i,tooltipPayloadSearcher:e,categoricalChartProps:r,ref:t}));export{m as B};
--- public/build/assets/index-PwqKpVXI.js
+++
@@ -1,1 +0,0 @@
-import{e as r}from"./app-2y4cu8g-.js";var o=r();export{o as r};
